ddc4cdb995dfe31b53b386f2535088d580836c07
Parminder Gill <pgill18@gmail.com>
2016-07-26 16:59:54 -0700

    commit

--- src/assign_contents.js
+++ src/assign_contents.js
@@ -6,3 +6,37 @@ document.getElementById("middle-head").innerHTML = flowPage.middle.head;
 document.getElementById("middle-body").innerHTML = flowPage.middle.body;
 document.getElementById("bottom-head").innerHTML = flowPage.bottom.head;
 document.getElementById("bottom-body").innerHTML = flowPage.bottom.body;
+
+var stateNextPageFromTopPost = 0;
+function nextPageFromTopPost() {
+  if(stateNextPageFromTopPost == 0) {
+    document.getElementById("top-body").innerHTML = flowPage.top.body + "      ...clicked";
+    stateNextPageFromTopPost = 1;
+  } else {
+    document.getElementById("top-body").innerHTML = flowPage.top.body;
+    stateNextPageFromTopPost = 0;   
+  }
+}
+var stateNextPageFromMiddlePost = 0;
+function nextPageFromMiddlePost() {
+  if(stateNextPageFromMiddlePost == 0) {
+    document.getElementById("middle-body").innerHTML = flowPage.middle.body + "      ...clicked";
+    stateNextPageFromMiddlePost = 1;
+  } else if(stateNextPageFromMiddlePost == 1) {
+    document.getElementById("middle-body").innerHTML = flowPage.middle.body + "      ...clicked again";
+    stateNextPageFromMiddlePost = 2;
+  } else {
+    document.getElementById("middle-body").innerHTML = flowPage.middle.body;
+    stateNextPageFromMiddlePost = 0;   
+  }
+}
+var stateNextPageFromBottomPost = 0;
+function nextPageFromBottomPost() {
+  if(stateNextPageFromBottomPost == 0) {
+    document.getElementById("bottom-body").innerHTML = flowPage.bottom.body + "      ...clicked";
+    stateNextPageFromBottomPost = 1;
+  } else {
+    document.getElementById("bottom-body").innerHTML = flowPage.bottom.body;
+    stateNextPageFromBottomPost = 0;   
+  }
+}
