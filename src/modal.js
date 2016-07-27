5db30ee10a441ff40847252c5ada041e138f8dcf
Parminder Gill <pgill18@gmail.com>
2016-07-27 06:06:36 -0700

    added map as a modal

--- src/modal.js
+++ src/modal.js
@@ -91,3 +91,31 @@
 	    }
 	}
 
+
+	// ----------------------------- Model 4 -----------------------------
+	// Get the modal
+	var modal4 = document.getElementById('myModal4');
+
+	// Get the button that opens the modal
+	var link4 = document.getElementById("modal4");
+
+	// Get the <span> element that closes the modal
+	var span4 = document.getElementsByClassName("close")[3];
+
+	// When the user clicks the button, open the modal
+	link4.onclick = function() {
+	    modal4.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span4.onclick = function() {
+	    modal4.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal4) {
+	        modal4.style.display = "none";
+	    }
+	}
+
